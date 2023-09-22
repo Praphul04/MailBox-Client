a08e67a05c2276f48ed731076a213a14ae447cc1
Praphul04 <praphulthakur6@gmail.com>
2023-09-22 10:38:30 +0530

    Signup Screen

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "../node_modules/react-bootstrap/dist/react-bootstrap"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import { Provider } from 'react-redux';
+import Store from "./store/Store"
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={Store}>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+    </BrowserRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
